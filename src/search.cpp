d8c91f4f9dd695ca4c1249f774d7a9ec2cbabf1a
Hanamuke <antoine.houdayer@laposte.net>
2019-01-07 19:32:30 +0100

    Tweak razoring Bench : 3014334

--- src/search.cpp
+++ src/search.cpp
@@ -765,8 +765,7 @@ namespace {
 
     // Step 7. Razoring (~2 Elo)
     if (   !rootNode // The required rootNode PV handling is not available in qsearch
-        &&  depth < 2 * ONE_PLY
-        &&  eval <= alpha - RazorMargin)
+        &&  eval <= alpha - (depth+1)/2 * RazorMargin)
         return qsearch<NT>(pos, ss, alpha, beta);
 
     improving =   ss->staticEval >= (ss-2)->staticEval
