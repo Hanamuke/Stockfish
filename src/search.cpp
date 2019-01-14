84909e46f65f11ae33684a11a5fb768f6a1889af
Houdayer Antoine <antoine.houdayer@laposte.net>
2019-01-14 15:20:01 +0100

    More update Bench : 3886311

--- src/search.cpp
+++ src/search.cpp
@@ -1202,7 +1202,7 @@ moves_loop: // When in check, search starts from here
         if (!pos.capture_or_promotion(bestMove))
             update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount,
                                stat_bonus(depth + (bestValue > beta + PawnValueMg ? ONE_PLY : DEPTH_ZERO)
-                                                + ((ss-1)->currentMove == MOVE_NULL ? ONE_PLY : DEPTH_ZERO)));
+                                                + 2 * ((ss-1)->currentMove == MOVE_NULL ? ONE_PLY : DEPTH_ZERO)));
 
         update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth + ONE_PLY));
 
