57dd74c670606384aa58cde7c1f4214545a2957f
Hanamuke <antoine.houdayer@laposte.net>
2019-01-14 20:02:09 +0100

    Do not give stat bonus to recapture of the moved piece Bench : 3788422

--- src/search.cpp
+++ src/search.cpp
@@ -1203,7 +1203,7 @@ moves_loop: // When in check, search starts from here
             update_quiet_stats(pos, ss, bestMove, quietsSearched, quietCount,
                                stat_bonus(depth + (bestValue > beta + PawnValueMg ? ONE_PLY : DEPTH_ZERO)));
 
-        update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth + ONE_PLY));
+        update_capture_stats(pos, bestMove, capturesSearched, captureCount, stat_bonus(depth + ((ss-1)->currentMove == MOVE_NULL || to_sq(bestMove) != to_sq((ss-1)->currentMove) ? ONE_PLY : DEPTH_ZERO)));
 
         // Extra penalty for a quiet TT or main killer move in previous ply when it gets refuted
         if (   ((ss-1)->moveCount == 1 || ((ss-1)->currentMove == (ss-1)->killers[0]))
